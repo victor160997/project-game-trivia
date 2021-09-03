55e9b3d12160949ce8d72efaf653137e6cde44b8
Victor Mendonca <victor.men.dev@gmail.com>
2021-09-03 16:43:16 -0300

    Reqs 12 e 13

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,37 +1,37 @@
-import React, { Component } from 'react';
-import HeaderFeedback from '../components/HeaderFeedback';
+// import React, { Component } from 'react';
+// import HeaderFeedback from '../components/HeaderFeedback';
 
-export default class Feedback extends Component {
-  constructor(props) {
-    super(props);
-    this.renderFeedback = this.renderFeedback.bind();
-  }
+// export default class Feedback extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.renderFeedback = this.renderFeedback.bind();
+//   }
 
-  renderFeedback(placar) {
-    const tres = 3;
-    if (placar < tres) {
-      return 'Podia ser melhor...';
-    }
-    return 'Mandou bem!';
-  }
+//   renderFeedback(placar) {
+//     const tres = 3;
+//     if (placar < tres) {
+//       return 'Podia ser melhor...';
+//     }
+//     return 'Mandou bem!';
+//   }
 
-  render() {
-    const placar = 0; // Essa variável placar é hipotética, ela vai ser apagada quando o tivermos o placar real retornado.
-    return (
-      <div>
-        <HeaderFeedback />
-        <span data-testid="feedback-text">
-          { () => this.renderFeedback(placar) }
-        </span>
-      </div>
-    );
-  }
-}
+//   render() {
+//     const placar = 0; Essa variável placar é hipotética, ela vai ser apagada quando o tivermos o placar real retornado.
+//     return (
+//       <div>
+//         <HeaderFeedback />
+//         <span data-testid="feedback-text">
+//           { () => this.renderFeedback(placar) }
+//         </span>
+//       </div>
+//     );
+//   }
+// }
 
-// o código abaixo vai ser usado para pegar o placar do estado global.
+// // o código abaixo vai ser usado para pegar o placar do estado global.
 
-/* const mapStateToProps = (state) => ({
-  placar: state.('valor de placar vindo do estado')
-});
+// const mapStateToProps = (state) => ({
+//   placar: state.('valor de placar vindo do estado')
+// });
 
-export default connect(mapStateToProps)(Feedback); */
+// export default connect(mapStateToProps)(Feedback);
