8716ddd9fc95ad5a839e30d12f487931b4b59399
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-09-08 16:09:20 -0300

    requisitos 05 a 08

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 import HeaderProfile from '../components/HeaderProfile';
+import './questions.css';
+import Stopwatch from './Stopwatch';
 
 class GamePage extends Component {
   constructor(props) {
@@ -12,10 +14,13 @@ class GamePage extends Component {
       show: false,
       index: 0,
       token: '',
+      timer: 30,
     };
 
     this.saveQuestionsInTheState = this.saveQuestionsInTheState.bind(this);
     this.requestQuestions = this.requestQuestions.bind(this);
+    this.trueOrFalse = this.trueOrFalse.bind(this);
+    this.handleTimer = this.handleTimer.bind(this);
   }
 
   componentDidMount() {
@@ -29,8 +34,20 @@ class GamePage extends Component {
     });
   }
 
+  handleTimer(count) {
+    this.setState({ timer: count });
+  }
+
+  trueOrFalse() {
+    document.querySelectorAll('.wrong-answer')
+      .forEach((value) => {
+        value.className = 'wrong';
+      });
+    document.querySelector('.correct-answer').className = 'correct';
+  }
+
   showQuestions() {
-    const { questions } = this.state;
+    const { questions, timer } = this.state;
     return questions.map((question, index) => (
       <div key={ index }>
         <p data-testid="question-category">{question.category}</p>
@@ -38,10 +55,15 @@ class GamePage extends Component {
         { question.incorrect_answers.concat(question.correct_answer).sort()
           .map((oneQuestion, questionIndex) => (
             <button
+              value={ oneQuestion }
               type="button"
               key={ questionIndex }
+              className={ oneQuestion === question.correct_answer
+                ? 'correct-answer' : 'wrong-answer' }
               data-testid={ oneQuestion === question.correct_answer
                 ? 'correct-answer' : `wrong-answer-${questionIndex}` }
+              onClick={ this.trueOrFalse }
+              disabled={ timer !== 0 ? false : 'true' }
             >
               {oneQuestion}
             </button>
@@ -65,6 +87,7 @@ class GamePage extends Component {
       <div>
         <HeaderProfile />
         {show ? this.showQuestions()[index] : <p>Loading...</p>}
+        <Stopwatch contador={ this.handleTimer } />
       </div>
     );
   }
